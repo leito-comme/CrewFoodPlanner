991e7a821baf500b0a2f54006e94db6464f8ef90
  Vladimir Kovshov <Vladimir@LEITOs-MacBook-Pro.local>
2025-05-02 15:24:34 +0300

    feat: added route for xls upload and changed model

--- backend/app/models/crew.py
+++ backend/app/models/crew.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from app.database import Base
 
 
@@ -7,5 +7,6 @@ class CrewMember(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    role = Column(String, nullable=False)
+    height = Column(Float, nullable=False)
+    weight = Column(Float, nullable=False)
     allergies = Column(String, nullable=True)
