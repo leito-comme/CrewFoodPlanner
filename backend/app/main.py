6731d30de6763ebf4256848112d52374e03c2134
  Vladimir Kovshov <Vladimir@LEITOs-MacBook-Pro.local>
2025-05-03 23:48:25 +0300

    feat: added db reset, fixed 307 redirect, added shipping router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from app.routers import crew
-from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+router = APIRouter(redirect_slashes=False)
+
+app = FastAPI(router=router)
 app.include_router(crew.router, tags=["crew"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
