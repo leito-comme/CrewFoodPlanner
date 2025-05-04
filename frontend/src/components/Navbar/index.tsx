2f5d2b00b524d425f775cfb94f412243402b1a18
  Vladimir Kovshov <Vladimir@LEITOs-MacBook-Pro.local>
2025-05-04 23:32:01 +0300

    feat: now using NavLink in navbar and shadcn btn in upload

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,7 +1,5 @@
-import { Tab, TabGroup, TabList } from "@headlessui/react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import clsx from "clsx";
-import { Fragment } from "react";
 
 const content = [
   {
@@ -34,28 +32,24 @@ function Navbar() {
   return (
     <div className="flex w-full bg-white px-6 py-4 justify-around">
       <div className="w-full max-w-6xl">
-        <TabGroup>
-          <TabList className="flex gap-6 justify-center">
-            {content.map(({ name, link }) => (
-              <Tab as={Fragment} key={name}>
-                {({ selected }) => (
-                  <Link
-                    to={link}
-                    className={clsx(
-                      "px-6 py-3 text-sm font-medium transition-all rounded-lg focus:outline-none",
-                      selected && "bg-blue-500 text-white", // Active tab styles
-                      !selected && "text-gray-600", // Default text color for inactive tabs
-                      !selected && "hover:text-blue-500", // Hover effect for non-selected tabs
-                      !selected && "hover:bg-blue-50" // Light background on hover for non-selected tabs
-                    )}
-                  >
-                    {name}
-                  </Link>
-                )}
-              </Tab>
-            ))}
-          </TabList>
-        </TabGroup>
+        <div className="flex gap-6 justify-center">
+          {content.map(({ name, link }) => (
+            <NavLink
+              key={name}
+              to={link}
+              className={({ isActive }) =>
+                clsx(
+                  "px-6 py-3 text-sm font-medium transition-all rounded-lg focus:outline-none",
+                  isActive
+                    ? "bg-blue-500 text-white"
+                    : "text-gray-600 hover:text-blue-500 hover:bg-blue-50"
+                )
+              }
+            >
+              {name}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );
