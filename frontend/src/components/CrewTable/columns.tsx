7dd7fbfcff29ed78e152acb9d89a33bacb6494a2
  Vladimir Kovshov <Vladimir@LEITOs-MacBook-Pro.local>
2025-05-04 23:31:04 +0300

    feat: added shadcn data table

---
+++ frontend/src/components/CrewTable/columns.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CrewMemberData } from "@/types";
+
+export const columns: ColumnDef<CrewMemberData>[] = [
+  {
+    accessorKey: "name",
+    header: () => <div className="text-left font-bold">Name</div>,
+  },
+  {
+    accessorKey: "height",
+    header: () => <div className="text-left font-bold">Height</div>,
+    cell: ({ row }) => {
+      const height = parseFloat(row.getValue("height"));
+      const formatted = `${height} cm`;
+      return <div className="text-left font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "weight",
+    header: () => <div className="text-left font-bold">Weight</div>,
+    cell: ({ row }) => {
+      const weight = parseFloat(row.getValue("weight"));
+      const formatted = `${weight} kg`;
+      return <div className="text-left font-medium">{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "allergies",
+    header: () => <div className="text-left font-bold">Allergies</div>,
+  },
+];
