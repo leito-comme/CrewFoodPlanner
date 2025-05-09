055391a8215ab76c926e8e7c5192ec9cdfca0cbb
Valdimir_MSI <None>
2025-05-09 23:45:12 +0300

    feat: added created and updated field and edited models file

--- backend/app/shipping/models.py
+++ backend/app/shipping/models.py
@@ -1,13 +1,17 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import DateTime, SqlEnum
+from sqlalchemy import DateTime, SqlEnum, Boolean
+
 from app.database import Base
 from app.constants.enum import Seasons
 
-class ShippingData(Base):
-	__tablename__ = "shipping_data"
 
-	id: Mapped[int] = mapped_column(primary_key=True, index=True)
-	voyage_number: Mapped[str] = mapped_column(nullable=False)
-	departure_time: Mapped[DateTime] = mapped_column(nullable=False)
-	arrival_time: Mapped[DateTime] = mapped_column(nullable=False)
-	season: Mapped[Seasons] = mapped_column(SqlEnum(Seasons), nullable=False)
+class ShippingData(Base):
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    voyage_number: Mapped[str] = mapped_column(nullable=False)
+    departure_time: Mapped[DateTime] = mapped_column(nullable=False)
+    arrival_time: Mapped[DateTime] = mapped_column(nullable=False)
+    season: Mapped[Seasons] = mapped_column(SqlEnum(Seasons), nullable=False)
+    is_current: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, server_default="false", index=True
+    )
