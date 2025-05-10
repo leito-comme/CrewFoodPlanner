615ba56e306c5e748b87f959ad2498a01a9e1863
Valdimir_MSI <None>
2025-05-11 00:38:27 +0300

    feat: expanded description text area, added shadcn component

--- frontend/src/components/InputForm/index.tsx
+++ frontend/src/components/InputForm/index.tsx
@@ -6,32 +6,43 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@components/ui/textarea';
 
 interface InputFormProps {
   placeholder: string;
   formName: string;
-  name: string
+  name: string;
 }
 
 function InputForm({ placeholder, formName, name }: InputFormProps) {
   const { control } = useFormContext();
 
   return (
-	<FormField
-	  control={control}
-	  name={name}
-	  render={({ field }) => (
-		<FormItem className="flex flex-col w-full">
-		  <FormLabel className="text-foreground text-sm">{formName}</FormLabel>
-		  <Input
-			className="bg-card"
-			id={name}
-			placeholder={placeholder}
-			{...field}></Input>
-		  <FormMessage className='text-[12px]'/>
-		</FormItem>
-	  )}
-	/>
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-col w-full">
+          <FormLabel className="text-foreground text-sm">{formName}</FormLabel>
+          {name === 'description' ? (
+            <Textarea
+              className="bg-card"
+              id={name}
+              placeholder={placeholder}
+              {...field}
+              rows={4}></Textarea>
+          ) : (
+            <Input
+              className="bg-card"
+              id={name}
+              placeholder={placeholder}
+              {...field}></Input>
+          )}
+
+          <FormMessage className="text-[12px]" />
+        </FormItem>
+      )}
+    />
   );
 }
 
