0d5f484544161acef15954d65064202a03e05693
Valdimir_MSI <None>
2025-05-10 23:51:11 +0300

    feat: added new styles - colors, grids

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 function Home() {
-  return <div className="">HOMEEEEEEEE</div>;
+  return <div className="p-4 m-2 flex flex-row gap-4 outline-1 outline-border rounded-xl bg-background h-[90vh]">HOMEEEEEEEE</div>;
 }
 
 export default Home;
