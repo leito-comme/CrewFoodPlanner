d64bb61f523ad681103bdaac0adb77a66d1b37b5
Valdimir_MSI <None>
2025-05-11 00:39:09 +0300

    feat: added check animation on form submit

--- frontend/src/pages/Shipping/index.tsx
+++ frontend/src/pages/Shipping/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, FormProvider } from 'react-hook-form';
 import { z } from 'zod';
@@ -6,51 +7,52 @@ import { Button } from '@/components/ui/button';
 import SeasonPicker from '@/components/SeasonPicker';
 import InputForm from '@/components/InputForm';
 import { DataTable } from '@/components/ShippingTable/data-table';
-import { columns } from '@/components/ShippingTable/columns'
+import { columns } from '@/components/ShippingTable/columns';
 import { ShippingData } from '@/types';
+import './Shipping.css';
 
 const shippingData: ShippingData[] = [
-	{
-	  voyage_number: "V123456",
-	  departure_date: "2025-05-01T10:00:00Z",
-	  arrival_date: "2025-05-05T18:00:00Z",
-	  season: "Summer",
-	  description: "A voyage from port A to port B",
-	  is_current: true,
-	},
-	{
-	  voyage_number: "V234567",
-	  departure_date: "2025-05-02T08:00:00Z",
-	  arrival_date: "2025-05-06T20:00:00Z",
-	  season: "Autumn",
-	  description: "Transporting cargo across the northern route",
-	  is_current: false,
-	},
-	{
-	  voyage_number: "V345678",
-	  departure_date: "2025-06-01T14:00:00Z",
-	  arrival_date: "2025-06-10T10:00:00Z",
-	  season: "Winter",
-	  description: "A long-distance journey with multiple stops",
-	  is_current: true,
-	},
-	{
-	  voyage_number: "V456789",
-	  departure_date: "2025-07-10T16:00:00Z",
-	  arrival_date: "2025-07-15T22:00:00Z",
-	  season: "Spring",
-	  description: "Seasonal route for perishable goods",
-	  is_current: false,
-	},
-	{
-	  voyage_number: "V567890",
-	  departure_date: "2025-08-01T12:00:00Z",
-	  arrival_date: "2025-08-07T14:00:00Z",
-	  season: "Summer",
-	  description: "Standard cargo transport",
-	  is_current: true,
-	},
-  ];
+  {
+    voyage_number: 'V123456',
+    departure_date: '2025-05-01T10:00:00Z',
+    arrival_date: '2025-05-05T18:00:00Z',
+    season: 'Summer',
+    description: 'A voyage from port A to port B',
+    is_current: true,
+  },
+  {
+    voyage_number: 'V234567',
+    departure_date: '2025-05-02T08:00:00Z',
+    arrival_date: '2025-05-06T20:00:00Z',
+    season: 'Autumn',
+    description: 'Transporting cargo across the northern route',
+    is_current: false,
+  },
+  {
+    voyage_number: 'V345678',
+    departure_date: '2025-06-01T14:00:00Z',
+    arrival_date: '2025-06-10T10:00:00Z',
+    season: 'Winter',
+    description: 'A long-distance journey with multiple stops',
+    is_current: true,
+  },
+  {
+    voyage_number: 'V456789',
+    departure_date: '2025-07-10T16:00:00Z',
+    arrival_date: '2025-07-15T22:00:00Z',
+    season: 'Spring',
+    description: 'Seasonal route for perishable goods',
+    is_current: false,
+  },
+  {
+    voyage_number: 'V567890',
+    departure_date: '2025-08-01T12:00:00Z',
+    arrival_date: '2025-08-07T14:00:00Z',
+    season: 'Summer',
+    description: 'Standard cargo transport',
+    is_current: true,
+  },
+];
 
 const FormSchema = z.object({
   number: z
@@ -74,21 +76,50 @@ const FormSchema = z.object({
 });
 
 function Shipping() {
+  const [animationClass, setAnimationClass] = useState('');
+  const [showCheckmark, setShowCheckmark] = useState(false);
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
+      number: '',
       dateRange: { from: undefined, to: undefined },
+      season: '',
       description: '',
     },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     console.log(data);
+    setShowCheckmark(true);
+    setAnimationClass('animate');
+
+    setTimeout(() => {
+      setAnimationClass('');
+      setShowCheckmark(false);
+    }, 1700);
   }
 
   return (
     <div className="p-4 m-2 flex flex-row gap-4 outline-1 outline-border rounded-xl bg-background h-[90vh]">
-      <div className="w-[360px] shrink-0 bg-card p-4 rounded-lg outline-1 outline-border">
+      <div className="w-[360px] shrink-0 bg-card p-4 rounded-lg outline-1 outline-border relative">
+        {showCheckmark && (
+          <div className={`overlay ${showCheckmark ? 'show' : ''}`}>
+            <div className="checkmark-wrapper">
+              <svg
+                viewBox="0 0 48 48"
+                xmlns="http://www.w3.org/2000/svg"
+                className={`checkmark ${animationClass}`}>
+                <path
+                  d="M 18 32.34 l -8.34 -8.34 -2.83 2.83 11.17 11.17 24 -24 -2.83 -2.83 z"
+                  stroke="#ddf2ff"
+                  fill="transparent"
+                />
+              </svg>
+            </div>
+          </div>
+        )}
+
         <FormProvider {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -112,14 +143,15 @@ function Shipping() {
             />
             <Button
               type="submit"
-              className="bg-primary text-white py-2 px-4 rounded">
+              className="bg-primary text-white py-2 px-4  rounded">
               Submit
             </Button>
           </form>
         </FormProvider>
       </div>
+
       <div className="flex-1">
-        <DataTable columns={columns} data={shippingData}/>
+        <DataTable columns={columns} data={shippingData} />
       </div>
     </div>
   );
