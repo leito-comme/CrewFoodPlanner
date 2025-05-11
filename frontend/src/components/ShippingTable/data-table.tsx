869cd61b79d71c72f42e3868809582000945226b
Valdimir_MSI <None>
2025-05-11 23:31:50 +0300

    feat: added overflow scroll and custom scroll styles for data table

--- frontend/src/components/ShippingTable/data-table.tsx
+++ frontend/src/components/ShippingTable/data-table.tsx
@@ -1,95 +1,90 @@
 import {
-	ColumnDef,
-	flexRender,
-	getCoreRowModel,
-	useReactTable,
-  } from "@tanstack/react-table";
-  
-  import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
-  } from "@/components/ui/table";
-  
-  import { cn } from "@/lib/utils";
-  
-  interface DataTableProps<TData, TValue> {
-	columns: ColumnDef<TData, TValue>[];
-	data: TData[];
-  }
-  
-  export function DataTable<TData, TValue>({
-	columns,
-	data,
-  }: DataTableProps<TData, TValue>) {
-	const table = useReactTable({
-	  data,
-	  columns,
-	  getCoreRowModel: getCoreRowModel(),
-	});
-  
-	return (
-	  <div className="rounded-lg border border-border shadow-sm overflow-hidden ">
-		<Table>
-		  <TableHeader>
-			{table.getHeaderGroups().map((headerGroup) => (
-			  <TableRow
-				key={headerGroup.id}
-				className="bg-primary/30 hover:bg-primary/30"
-			  >
-				{headerGroup.headers.map((header) => (
-				  <TableHead
-					key={header.id}
-					className="text-secondary-foreground/70 font-semibold text-sm px-4 py-3"
-				  >
-					{header.isPlaceholder
-					  ? null
-					  : flexRender(
-						  header.column.columnDef.header,
-						  header.getContext()
-						)}
-				  </TableHead>
-				))}
-			  </TableRow>
-			))}
-		  </TableHeader>
-		  <TableBody className="bg-card">
-			{table.getRowModel().rows?.length ? (
-			  table.getRowModel().rows.map((row) => (
-				<TableRow
-				  key={row.id}
-				  className={cn(
-					"even:bg-muted/50 transition-colors hover:bg-primary/10",
-					row.getIsSelected() && "bg-primary/30"
-				  )}
-				  data-state={row.getIsSelected() && "selected"}
-				>
-				  {row.getVisibleCells().map((cell) => (
-					<TableCell
-					  key={cell.id}
-					  className="text-secondary-foreground/70 text-sm font-semibold px-4 py-3"
-					>
-					  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-					</TableCell>
-				  ))}
-				</TableRow>
-			  ))
-			) : (
-			  <TableRow>
-				<TableCell
-				  colSpan={columns.length}
-				  className="h-24 text-center text-muted-foreground"
-				>
-				  No results.
-				</TableCell>
-			  </TableRow>
-			)}
-		  </TableBody>
-		</Table>
-	  </div>
-	);
-  }
-  
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+import { cn } from '@/lib/utils';
+import './ShippingTable.css'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-lg bg-card border border-border shadow-sm overflow-y-scroll h-full">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow
+              key={headerGroup.id}
+              className="bg-primary/30 hover:bg-primary/30">
+              {headerGroup.headers.map((header) => (
+                <TableHead
+                  key={header.id}
+                  className="sticky top-0 text-foreground font-semibold text-sm px-4 py-3">
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody className="bg-card h-full">
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                className={cn(
+                  'even:bg-muted/50 transition-colors hover:bg-primary/10',
+                  row.getIsSelected() && 'bg-primary/30'
+                )}
+                data-state={row.getIsSelected() && 'selected'}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell
+                    key={cell.id}
+                    className="text-secondary-foreground/70 text-sm font-semibold px-4 py-3">
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={columns.length}
+                className="h-24 text-center text-muted-foreground">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
